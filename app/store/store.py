6ffb7f3ff97bd0862b0d76de16832fd6819b3a13
yarayk <yaroslav.bogdanov.2005@mail.ru>
2026-03-10 20:32:43 +0500

    game: add game accessor and service

--- app/store/store.py
+++ app/store/store.py
@@ -7,13 +7,17 @@ if TYPE_CHECKING:
 
 
 class Store:
-    def __init__(self, app: Application):
-        from app.store.database.database import Database
+    def __init__(self, app: "Application"):
+        from app.users.accessor import UserAccessor
         from app.tg.client import TgClient
         from app.tg.poller import Poller
-        from app.users.accessor import UserAccessor
+        from app.store.database.database import Database
+        from app.game.accessor import GameAccessor
+        from app.game.service import GameService
 
         self.user = UserAccessor(self)
         self.tg_client = TgClient(app)
         self.poller = Poller(app)
         self.database = Database(app)
+        self.game = GameAccessor(app)
+        self.game_service = GameService(app)
